e2eb5eec02b048577ed939bb8b33d09001c55fc5
Marcus Pereira <marcus.work@hotmail.com>
2020-04-14 15:32:32 -0300

    Webpack config

--- public/bundle.js
+++ public/bundle.js
@@ -90,10 +90,23 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-eval("var soma = function soma(x, y) {\n  return x + y;\n};\n\nconsole.log(soma(2, 10));\n\n//# sourceURL=webpack:///./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _soma__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./soma */ \"./src/soma.js\");\n\nconsole.log(Object(_soma__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(2, 10));\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/soma.js":
+/*!*********************!*\
+  !*** ./src/soma.js ***!
+  \*********************/
+/*! exports provided: soma */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nvar soma = function soma(x, y) {\n  return x + y;\n};\n\n//# sourceURL=webpack:///./src/soma.js?");
 
 /***/ })
 
