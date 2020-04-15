e040f95ad11e9d37a9c6e29cf6313037afdd79c6
Marcus Pereira <marcus.work@hotmail.com>
2020-04-15 10:18:04 -0300

    props children

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Header from "./components/Header";
 const App = () => {
   return (
     <>
-      <Header title="Prop 1" />
-      <Header title="Prop 2" />
+      <Header title="Prop 1">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Prop 2">
+        <ul>
+          <li>menu 1</li>
+          <li>menu 2</li>
+          <li>menu 3</li>
+        </ul>
+      </Header>
     </>
   );
 };
